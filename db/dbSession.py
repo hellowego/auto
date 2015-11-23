8298e0fa79a2bd58e492b3840e477b701cf29164
hellowego <hellowego@gmail.com>
2015-11-23 20:04:49 +0800

    修改数据库为aws的mysql数据库

--- db/dbSession.py
+++ db/dbSession.py
@@ -5,7 +5,9 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 
 BaseModel = declarative_base()
-engine = create_engine('mysql://auto:auto@localhost:3306/auto',connect_args={'charset':'utf8'})
+
+#engine = create_engine('mysql://auto:auto@localhost:3306/auto',connect_args={'charset':'utf8'})
+engine = create_engine('mysql://auto:auto@54.179.147.230:3306/auto',connect_args={'charset':'utf8'})
 DBSession = sessionmaker(bind=engine)
 
 
