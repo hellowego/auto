981943e095c22f614ca7e6b08cba995f3256d863
hellowego <hellowego@gmail.com>
2016-06-29 23:13:57 +0800

    投票箭头css功能完成

--- static/js/auto.js
+++ static/js/auto.js
@@ -3,8 +3,47 @@ var AUTO =
 	// 赞成投票
 	agree_vote: function(selector, user_name, answer_id)
 	{
-		$(selector).removeClass('up');
-		$(selector).addClass('upmod');
+		// 状态未投票
+		if($(selector).parent().hasClass("unvoted")){
+			$(selector).removeClass('up').addClass('upmod');
+			$(selector).parent().addClass('likes').removeClass('unvoted');
+		}
+		// 状态为喜欢
+		else if($(selector).parent().hasClass("likes")){
+			$(selector).removeClass('upmod').addClass('up');
+			$(selector).parent().addClass('unvoted').removeClass('likes');
+		}
+		// 状态为dislike
+		else if($(selector).parent().hasClass("dislikes")){
+			$(selector).removeClass('up').addClass('upmod');
+			$(selector).parent().addClass('likes').removeClass('dislikes');
+			$(selector).parent().find("i.downmod").removeClass('downmod').addClass('down');
+		}
+
+		
+	},
+
+	disAgree_vote: function(selector, user_name, answer_id)
+	{
+		// 状态未投票
+		if($(selector).parent().hasClass("unvoted")){
+			$(selector).removeClass('down').addClass('downmod');
+			$(selector).parent().addClass('dislikes');
+			$(selector).parent().removeClass('unvoted');
+		}
+		// 状态为喜欢
+		else if($(selector).parent().hasClass("likes")){
+			$(selector).removeClass('down').addClass('downmod');
+			$(selector).parent().removeClass('likes').addClass('dislikes');
+			$(selector).parent().find("i.upmod").removeClass('upmod').addClass('up');		
+		}
+		// 状态为dislike
+		else if($(selector).parent().hasClass("dislikes")){
+			$(selector).removeClass('downmod').addClass('down');
+			$(selector).parent().removeClass('dislikes').addClass('unvoted');
+		}
+
+		
 	}
 
 };
