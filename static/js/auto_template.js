678f5e022b2b74a1d6aa3298d598a661d62022a0
hellowego <hellowego@gmail.com>
2016-10-17 19:18:47 +0800

    add templatebox

--- static/js/auto_template.js
+++ static/js/auto_template.js
@@ -3,6 +3,23 @@ var AUTO_TEMPLATE = {
 	'loadingBox':
 		'<div id="aw-loading" class="hide">'+
 			'<div id="aw-loading-box"></div>'+
-		'</div>'
+		'</div>',
+
+	'commentBox':
+	<form action="#" class="usertext cloneable warn-on-unload" onsubmit="return post_form(this, 'comment')"
+id="commentreply_t1_d7wco6o" style="">
+	<div class="usertext-edit md-container" style="width: 500px;">
+	<div class="md">
+	<textarea rows="1" cols="1" name="text" class="" data-event-action="comment" data-type="link"
+style="width: 500px; height: 100px;"></textarea>
+	</div>
+	<div class="bottom-area">
+	<div class="usertext-buttons">
+	<button type="submit" onclick="" class="save">save</button>
+	<button type="button" onclick="return cancel_usertext(this);" class="cancel" style="">cancel</button>
+	</div>
+	</div>
+	</div>
+	</form>
 	
 }
